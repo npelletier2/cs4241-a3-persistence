808e2c3a4a1751f0fe4911a48a48e89b41e96dc7
Niko Pelletier <npelletier3435@gmail.com>
2021-09-26 23:50:41 -0400

    Add, edit, and delete functionality

--- public/main/script.js
+++ public/main/script.js
@@ -1,33 +1,46 @@
-//create cookies object
-const cookies = {};
-const cookieArr = document.cookie.split(";");
-cookieArr.forEach((value, i, arr) => {
-    cookieArr[i] = decodeURI(value).trim().split("=");
-    cookies[cookieArr[i][0]] = cookieArr[i][1];
-})
-
-const user = cookies.user.split(" ")[0];
-
-//set title bar text
+//get username from server
 const titleBar = document.getElementsByClassName("title-bar-text").item(0);
-titleBar.innerText = `Orders: ${user}`;
-
 const orderBox = document.getElementsByClassName("order-box").item(0);
 
-let body = JSON.stringify({user});
-
-fetch("/user-orders", {
-    method: "POST",
-    headers: {"Content-Type": "application/json"},
-    body
-})
+fetch("/username")
 .then(response => response.json())
-.then((orders) => {
-    orderBox.innerText = "Click on an order to edit it:";
-    orders.forEach((order) => {
-        orderBox.append(createOrderNode(order));
+.then((data) => {
+    user = data.user;
+
+    //set title bar text
+    titleBar.innerText = `Orders: ${user}`;
+
+    let body = JSON.stringify({user});
+
+    fetch("/user-orders", {
+        method: "POST",
+        headers: {"Content-Type": "application/json"},
+        body
+    })
+    .then(response => response.json())
+    .then((orders) => {
+        orderBox.innerHTML = "<h4>Click an order to edit</h4>";
+        orders.forEach((order) => {
+            orderBox.append(createOrderNode(order));
+        })
+        
+        const addButton = document.createElement("button");
+        addButton.setAttribute("type", "button");
+        addButton.setAttribute("onclick", "addOrder()");
+        addButton.setAttribute("id", "add-button");
+        addButton.innerText = "Add order";
+        orderBox.append(addButton);
     })
 })
+
+
+function editOrder(num){
+    fetch(`/edit-order.html?num=${num}`).then((res) => window.location.assign(res.url));
+}
+
+function addOrder(){
+    fetch(`edit-order.html?num=new`).then((res) => window.location.assign(res.url));
+}
 
 //helper to create a node from order info
 function createOrderNode(order){
@@ -38,9 +51,9 @@ function createOrderNode(order){
         <p><em>Cone:</em> ${order.cone}</p>
         <p><em>Notes:</em> ${order.notes}</p>`
 
-    orderNode = document.createElement("div");
+    orderNode = document.createElement("button");
     orderNode.setAttribute("class", "order");
-    orderNode.setAttribute("id", `order-${order.num}`);
+    orderNode.setAttribute("onclick", `editOrder(${order.num})`);
     orderNode.innerHTML = orderHTML;
 
     return orderNode;
--- server.js
+++ server.js
@@ -2,18 +2,13 @@ require("dotenv").config();
 const mongodb = require("mongodb");
 const crypto = require("crypto");
 const cookie = require("cookie-session");
+const favicon = require("serve-favicon");
 const express = require("express");
-const { response } = require("express");
+const { SSL_OP_DONT_INSERT_EMPTY_FRAGMENTS } = require("constants");
 const app = express();
 
-const dreams = [
-  "Find and count some sheep",
-  "Climb a really tall mountain",
-  "Wash the dishes"
-];
-
-
 //middleware
+app.use(favicon(__dirname + "/public/favicon.ico"));
 app.use(express.static(__dirname + "/public"));
 app.use(express.json());
 
@@ -29,7 +24,8 @@ app.use(express.urlencoded({extended: true}));
 app.use(cookie({
   name: "session",
   keys: [crypto.randomBytes(20).toString("hex"), crypto.randomBytes(20).toString("hex")]
-}))
+}));
+
 
 
 //connect to mongodb
@@ -56,19 +52,44 @@ app.get("/", (request, response) => {
   response.sendFile(__dirname + "/views/index.html");
 });
 
-app.get("/dreams", (request, response) => {
-  response.json(dreams);
-});
-
 app.get("/main.html", (req, res) => {
   res.sendFile(__dirname + "/views/main.html");
+})
+
+//get the username of the user associated with this session
+app.get("/username", (req, res) => {
+  res.json({user: req.session.user});
+})
+
+app.get("/edit-order.html", (req, res) => {
+  res.sendFile(__dirname + "/views/edit-order.html");
+})
+
+app.get("/order-info", (req, res) => {
+  const num = req.query.num;
+  const user = req.session.user;
+
+  userData.find({user}).toArray()
+  .then((data) => {
+    const orders = data[0].orders;
+    let order = null;
+    
+    for(let i = 0; i < orders.length; i++){
+      if(orders[i].num.toString() === num){
+        order = orders[i];
+        break;
+      }
+    }
+
+    res.json(order);
+  })
 })
 
 
 //post requests
 app.post("/login", (req,res) => {
-  username = req.body.username;
-  password = req.body.password;
+  const username = req.body.username;
+  const password = req.body.password;
   
   users.find({
     username,
@@ -77,20 +98,123 @@ app.post("/login", (req,res) => {
   .toArray()
   .then((data) => {
     if(data.length === 1){//login success
-      res.cookie("user", username + " " + password)
-         .redirect("main.html");
+      req.session.user = username;
+      req.session.pass = password;
+      res.redirect("main.html");
     }else{
       res.redirect("/");
     }
   })
 })
 
+//get a list of orders for a specified user
 app.post("/user-orders", (req,res) => {
-  console.log(req.body);
   userData.find(req.body).toArray()
   .then((data) => {
-    console.log(data[0].orders);
     res.json(data[0].orders);
+  })
+})
+
+//req.body = {num: <order number>}
+app.post("/delete-order", (req, res) => {
+  userData.find({user: req.session.user}).toArray()
+  .then((data) => {
+    let orders = data[0].orders;
+    const id = data[0]._id.toString();
+
+
+    for(let i = 0; i < orders.length; i++){
+      if(orders[i].num.toString() === req.body.num.toString()){
+        orders = orders.filter(item => item !== orders[i]);
+      }
+    }
+
+    userData.updateOne(
+      {_id:mongodb.ObjectId(id)},
+      {$set:{orders}}
+    )
+    .then(result => res.json(result));
+  })
+})
+
+//req.body = {flavor, toppings, cone, notes}
+app.post("/add-order", (req, res) => {
+  userData.find({user: req.session.user}).toArray()
+  .then((data) => {
+    let orders = data[0].orders;
+    const id = data[0]._id.toString()
+
+    //find the lowest available number
+    orders.sort(function(a, b){return a.num-b.num});
+    let num = orders.length+1;
+    for(let i = 0; i < orders.length; i++){
+      if(orders[i].num !== (i+1)){
+        num = i+1
+        break;
+      }
+    }
+
+    orders.splice(num-1, 0, {
+      num,
+      flavor: req.body.flavor,
+      toppings: req.body.toppings,
+      cone: req.body.cone,
+      notes: req.body.notes
+    })
+
+    userData.updateOne(
+      {_id:mongodb.ObjectId(id)},
+      {$set:{orders}}
+    )
+    .then(result => res.json(result));
+  })
+})
+
+//subit an edit to the database
+//req.body = {num, flavor, toppings, cone, notes}
+app.post("/submit-edit", (req, res) => {
+  userData.find({user: req.session.user}).toArray()
+  .then((data) => {
+    let orders = data[0].orders;
+    const id = data[0]._id.toString()
+    
+    for(let i = 0; i < orders.length; i++){
+      if(orders[i].num.toString() === req.body.num.toString()){
+        orders[i] = {
+          num: req.body.num,
+          flavor: req.body.flavor,
+          toppings: req.body.toppings,
+          cone: req.body.cone,
+          notes: req.body.notes
+        }
+        break;
+      }
+    }
+
+    userData.updateOne(
+      {_id:mongodb.ObjectId(id)},
+      {$set:{orders}}
+    )
+    .then(result => res.json(result));
+  })
+})
+
+//middleware
+app.use((req,res,next) => {
+  username = req.session.user;
+  password = req.session.pass;
+  
+  users.find({
+    username,
+    password
+  })
+  .toArray()
+  .then((data) => {
+    if(data.length === 1){//valid cookie
+      next()
+    }else{//invalid cookie, send user to login page
+      res.sendFile(__dirname + "/views/index.html");
+    }
   })
 })
 
